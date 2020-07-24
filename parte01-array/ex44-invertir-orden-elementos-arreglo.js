f3f6ea1559eb8cb40089a3103c30f6b6d81cf7f5
infzer0 <johnortizo@outlook.com>
2020-07-24 17:28:05 -0500

    Lodash Ejercicio: 44 Invertir el Orden de los Elementos de un Arreglo con la Función reverse()
    Invertir el Orden de los Elementos de un Arreglo con la Función reverse() con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte01-array/ex44-invertir-orden-elementos-arreglo.js
@@ -0,0 +1,13 @@
+// Ejercicio 44: Invertir el orden de los elementos de un arreglo con la función reverse().
+
+const _ = require('lodash');
+
+let numeros = [1, 2, 3, 4, 5];
+
+console.log(numeros);
+
+console.log();
+
+_.reverse(numeros);
+
+console.log(numeros);
