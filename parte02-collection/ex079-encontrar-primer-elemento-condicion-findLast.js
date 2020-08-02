7615495c7131418f1b82afb091ecec37b20c000d
infzer0 <johnortizo@outlook.com>
2020-08-02 12:51:00 -0500

    Lodash Ejercicio: 79 Encontrar el Primer Elemento que Cumpla un Predicado con la Función findLast()
    Encontrar el Primer Elemento que Cumpla un Predicado con la Función findLast() con el lenguaje de programación JavaScript y la librería Lodash.

--- parte02-collection/ex079-encontrar-primer-elemento-condicion-findLast.js
+++ parte02-collection/ex079-encontrar-primer-elemento-condicion-findLast.js
@@ -1,3 +1,28 @@
 // Ejercicio 79: Encontrar el primer elemento que cumpla un predicado con la función findLast().
 
+// find(): izquierda -> derecha
+// findLast(): derecha -> izquierdo
 
+const _ = require('lodash');
+
+let numeros = [3, 7, 9, 8, 5, 2, 11];
+
+let resultado = _.findLast(numeros, (n) => n >= 5);
+console.log(resultado); // 11
+
+console.log();
+
+resultado = _.findLast(numeros, (n) => n >= 5, 5);
+console.log(resultado); // 5
+
+console.log();
+
+let personas = [{id: 1001, nombre: 'Edward', edad: 29}, {id: 1002, nombre: 'Daniela', edad: 23}, {id: 1003, nombre: 'Jorge', edad: 23}];
+
+resultado = _.findLast(personas, (p) => p.edad == 23);
+console.log(resultado);
+
+console.log();
+
+resultado = _.findLast(personas, ['edad', 23]);
+console.log(resultado);
