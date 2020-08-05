3f89ebb9e8e63267141b7227ac1e82644914d5f2
infzer0 <johnortizo@outlook.com>
2020-08-05 11:36:41 -0500

    Lodash Ejercicio: 101 Obtener la Marca de Tiempo Actual en Milisegundos con la Función now()
    Obtener la Marca de Tiempo Actual en Milisegundos con la Función now() con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte02-collection/ex101-marca-tiempo-milisegundos-now.js
@@ -0,0 +1,9 @@
+// Ejercicio 101: Obtener la marca de tiempo actual en milisegundos con la función now().
+
+const _ = require('lodash');
+
+// Época Unix - 1 Enero de 1970 00:00:00 (UTC)
+
+let milisegundos = _.now()
+
+console.log(milisegundos);
