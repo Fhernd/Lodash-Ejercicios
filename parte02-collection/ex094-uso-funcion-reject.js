1b1869dc83f63a879188fa9a5864fc462c90e14b
infzer0 <johnortizo@outlook.com>
2020-08-05 07:03:10 -0500

    Lodash Ejercicio: 94 Demostrar el Uso de la Función reject() sobre una Colección de Objetos
    Demostrar el Uso de la Función reject() sobre una Colección de Objetos con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte02-collection/ex094-uso-funcion-reject.js
@@ -0,0 +1,28 @@
+// Ejercicio 94: Demostrar el uso de la función reject() sobre una colección de objetos.
+
+const _ = require('lodash');Lodash Ejercicio: 94 Demostrar el Uso de la Función reject() sobre una Colección de Objetos
+
+let personas = [
+    {id: 1001, nombre: 'Danny', activo: true},
+    {id: 1002, nombre: 'Alexander', activo: false},
+    {id: 1003, nombre: 'Sonia', activo: true},
+    {id: 1004, nombre: 'Juan', activo: true},
+    {id: 1004, nombre: 'Angela', activo: false}
+];
+
+console.log(personas);
+
+console.log();
+
+let personasActivas = _.reject(personas, (p) => p.activo);
+console.log(personasActivas);
+
+console.log();
+
+personasActivas = _.reject(personas, 'activo');
+console.log(personasActivas);
+
+console.log();
+
+personasActivas = _.reject(personas, ['activo', true]);
+console.log(personasActivas);
