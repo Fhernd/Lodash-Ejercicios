01f38393f17aba10df426c92922c1da3c777346f
infzer0 <johnortizo@outlook.com>
2021-02-03 14:13:26 -0500

    Lodash Ejercicio: 250 Demostrar el Uso Básico de la Función chain()
    Demostrar el Uso Básico de la Función chain() con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte09-seq/ex250-demo-uso-funcion-chain().js
@@ -0,0 +1,16 @@
+// Ejercicio 250: Demostrar el uso básico de la función chain().
+
+const _ = require('lodash');\
+
+let computadores = [
+    {id: 1001, marca: 'Clone', cpu: 'Intel Core i7'},
+    {id: 1002, marca: 'MSi', cpu: 'Intel Core i7'},
+    {id: 1003, marca: 'Apple', cpu: 'Intel Core i5'}
+];
+
+console.log(computadores);
+
+console.log();
+
+let resultado = _(computadores).chain().head().pick('marca').value();
+console.log(resultado);
