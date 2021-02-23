b84b5cd70c56d34d0c0dfb705943905b8ec1c936
infzer0 <johnortizo@outlook.com>
2021-02-23 11:39:57 -0500

    Lodash Ejercicio: 271 Usar la Función replace() para Reemplazar Texto en una Cadena de Caracteres
    Usar la Función replace() para Reemplazar Texto en una Cadena de Caracteres con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte10-string/ex271-demo-uso-funcion-replace().js
@@ -0,0 +1,13 @@
+// Ejercicio 271: Usar la función replace() para reemplazar texto en una cadena de caracteres.
+
+const _ = require('lodash');
+
+let frase = 'javaScrIPT es tremendo';
+console.log(frase);
+
+console.log();
+
+// JavaScript
+
+let resultado = _.replace(frase, 'javaScrIPT', 'JavaScript');
+console.log(resultado); // JavaScript
