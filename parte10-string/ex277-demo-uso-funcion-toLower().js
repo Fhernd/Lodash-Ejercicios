922432fac459e0fe5a2f50df43bde50366b64db6
infzer0 <johnortizo@outlook.com>
2021-02-28 12:23:52 -0500

    Lodash Ejercicio: 277 Usar la Función toLower() para Dejar en Minúscula el Contenido de un String
    Usar la Función toLower() para Dejar en Minúscula el Contenido de un String con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte10-string/ex277-demo-uso-funcion-toLower().js
@@ -0,0 +1,7 @@
+// Ejercicio 277: Usar la función toLower() para dejar en minúscula el contenido de un objeto String.
+
+const _ = require('lodash');
+
+console.log(_.toLower('JavaScript Es Genial')); // javascript es genial
+console.log(_.toLower('JavaScript-Es-Genial')); // javascript-es-genial
+console.log(_.toLower('JAVASCRIPT ES GENIAL')); // javascript es genial
