e07a0bb4f4ee247b66b02ab2f5b3a2008887a5b1
infzer0 <johnortizo@outlook.com>
2021-03-01 15:22:22 -0500

    Lodash Ejercicio 278: Usar la Función toUpper() para Dejar en Mayúscula un Objeto Tipo String
    Usar la Función toUpper() para Dejar en Mayúscula un Objeto Tipo String con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte10-string/ex278-demo-uso-funcion-toUpper().js
@@ -0,0 +1,7 @@
+// Ejercicio 278: Usar la función toUpper() para dejar en mayúscula el contenido de un objeto String.
+
+const _ = require('lodash');
+
+console.log(_.toUpper('¡JavaScript es tremendo!')); // ¡JAVASCRIPT ES TREMENDO!
+console.log(_.toUpper('¡javascript es tremendo!')); // ¡JAVASCRIPT ES TREMENDO!
+console.log(_.toUpper('***¡javascript es tremendo!***')); // ***¡JAVASCRIPT ES TREMENDO!***
