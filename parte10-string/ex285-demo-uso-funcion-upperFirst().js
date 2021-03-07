d930bf8e73601e743e38c39fba7fe83f1a9d270f
infzer0 <johnortizo@outlook.com>
2021-03-07 14:59:47 -0500

    Lodash Ejercicio: 275 Usar la Función upperFirst() para Dejar en Mayúscula la Primera Letra de un Texto
    Usar la Función upperFirst() para Dejar en Mayúscula la Primera Letra de un Texto con el lenguaje de programación Python.

---
+++ parte10-string/ex285-demo-uso-funcion-upperFirst().js
@@ -0,0 +1,7 @@
+// Ejercicio 285: Usar la función upperFirst() para dejar en mayúscula la primera letra de una cadena.
+
+const _ = require('lodash');
+
+console.log(_.upperFirst('lodash'));    // Lodash
+console.log(_.upperFirst('lodash es tremendo'));    // Lodash es tremendo
+console.log(_.upperFirst('1 lodash es tremendo'));    // 1 lodash es tremendo
