ff4cfbd40e7f82b9713805d186e2c996b5ad6a1c
infzer0 <johnortizo@outlook.com>
2021-03-18 17:07:34 -0500

    Lodash Ejercicio: 294 Usar la Función iteratee() como Soporte para Filtrar Datos en Consultas
    Usar la Función iteratee() como Soporte para Filtrar Datos en Consultas con el lenguaje de programación JavaScript y la librería Lodash.

---
+++ parte11-util/ex296-demo-uso-funcion-iteratee().js
@@ -0,0 +1,33 @@
+// Ejercicio 296: Usar la función iteratee() como soporte para filtrar datos.
+
+const _ = require('lodash');
+
+let computadores = [
+    {id: 1001, marca: 'MSi', ram: 32, ssd: 1024},
+    {id: 1002, marca: 'Apple', ram: 16, ssd: 512},
+    {id: 1003, marca: 'Clone', ram: 128, ssd: 720},
+    {id: 1004, marca: 'HP', ram: 128, ssd: 720, board: 'Asus'}
+];
+
+let resultado = _.filter(computadores, _.iteratee({marca: 'Apple'}));
+console.log(resultado);
+
+console.log();
+
+resultado = _.filter(computadores, _.iteratee({marca: 'Apple', ram: 8}));
+console.log(resultado); // []
+
+console.log();
+
+resultado = _.filter(computadores, _.iteratee('marca'));
+console.log(resultado); // Todos
+
+console.log();
+
+resultado = _.filter(computadores, _.iteratee('board'));
+console.log(resultado); // []
+
+console.log();
+
+resultado = _.map(computadores, _.iteratee('marca'));
+console.log(resultado); // ['MSi', 'Apple', 'Clone', 'HP']
